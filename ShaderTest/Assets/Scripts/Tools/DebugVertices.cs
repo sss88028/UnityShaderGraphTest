9a6b856fdb7b75eaf1400409792431ce326a8df9
JackFunfia <jack.tu@funfia.com>
2023-03-06 13:51:09 +0800

    Simple Vertice test

---
+++ ShaderTest/Assets/Scripts/Tools/DebugVertices.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DebugVertices : MonoBehaviour {
+
+	Mesh mesh;
+	Vector3[] vertices;
+
+	void OnDrawGizmos()
+	{
+		if (vertices == null)
+		{
+			mesh = GetComponent<MeshFilter>().sharedMesh;
+			vertices = mesh.vertices;
+		}
+		foreach (Vector3 v in vertices)
+            UnityEditor.Handles.Label(transform.position + v, "v: " + v.ToString());
+	}
+}
